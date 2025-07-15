0e0e8221adb82d4f071f41e5d9896b778afc2df5
swapnilkature123 <swapnilkature917@gmail.com>
2025-07-15 16:52:44 +0530

    DSA logical programs

--- src/com/striver/Arrays/MovesZeroToEnd.java
+++ src/com/striver/Arrays/MovesZeroToEnd.java
@@ -3,31 +3,41 @@ package com.striver.Arrays;
 import java.util.Arrays;
 import java.util.Vector;
 
+// Moves all zeros to the end of the array
 public class MovesZeroToEnd {
 
 	public static void main(String[] args) {
-		int arr[]= {1,0,2,3,2,0,0,4,5,1};
-		int n=arr.length;
-//		Brute approach
-		// step 1
-		Vector<Integer> temp=new Vector<>();
-		for (Integer i : arr) {
-			if(arr[i]!=0) {
-				temp.add(i);
+		int arr[] = { 1, 0, 2, 3, 2, 0, 0, 4, 5, 1 };
+		int n = arr.length;
+//		Brute force approach  TC : O(2N)  SC : O(N)
+		// Step 1: Store all non-zero elements in temp
+		Vector<Integer> temp = new Vector<>();
+		for (int i = 0; i < n; i++) {
+			if (arr[i] != 0) {
+				temp.add(arr[i]);
 			}
 		}
-		System.out.println(temp);
-		// step 2
-		for(int i=0; i<temp.size(); i++) {
-			for(Integer t:temp) {
-				arr[t]=t;
-			}
+		System.out.println("Non-zero elements: " + temp);
+
+		// Step 2: Copy back the non-zero elements
+		for (int i = 0; i < temp.size(); i++) {
+			arr[i] = temp.get(i);
 		}
-		
-		System.out.println(Arrays.toString(arr));
-		
-		
-		
+
+		// Step 3: Fill remaining array with zeros
+		for (int i = temp.size(); i < n; i++) {
+			arr[i] = 0;
+		}
+
+		// Final result
+		System.out.println("Result after moving zeros: " + Arrays.toString(arr));
+
 	}
 
 }
+/*
+Non-zero elements: [1, 2, 3, 2, 4, 5, 1]
+Result after moving zeros: [1, 2, 3, 2, 4, 5, 1, 0, 0, 0]
+
+
+*/
---
+++ src/com/striver/Arrays/MovesZeroToEndOptimalSol.java
@@ -0,0 +1,39 @@
+package com.striver.Arrays;
+
+import java.util.Arrays;
+
+
+public class MovesZeroToEndOptimalSol {
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 0, 2, 3, 2, 0, 0, 4, 5, 1 };
+		int n = arr.length;
+		
+		int j=-1;
+		for(int i=0; i<n; i++)
+		{
+			if(arr[i]==0) {
+				j=i;
+				break;
+			}
+		}
+		
+		for(int i=j+1; i<n; i++) {
+			if(arr[i]!=0) {
+				swap(arr,i, j);
+				j++;
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));  //[1, 2, 3, 2, 4, 5, 1, 0, 0, 0]
+
+	}
+	
+	public static void swap(int arr[],int i,int j) {
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+		
+	}
+	
+}
